57af2656e593e34fe471092a9641d1aaff330eb6
AdityaModi001 <adityamodi108@gmail.com>
2025-12-02 01:14:55 +0530

    Second Version of the data Saved !!!

--- mycode.py
+++ mycode.py
@@ -8,6 +8,17 @@ data = {'name':['Alice','Bob','Charlie'],
 df = pd.DataFrame(data)
 
 
+# Adding new row to df for V2
+new_row_loc = {'name': 'GF1', 'Age': 20, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
+
+# Adding new row to df for V3df = pd.DataFrame(data)
+# new_row_loc2 = {'Name': 'V3',
+#                 'Age': 30,
+#                 'City': 'City1'}
+# df.loc[len(df.index)] = new_row_loc2
+
+
 
 # Ensures that 'data' directory exits at the root level
 data_dir = 'data'  # this is the folder name !!
